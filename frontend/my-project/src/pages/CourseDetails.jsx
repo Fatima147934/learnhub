704b207189931f9796e1c7175700d29a1468ba86
Fatima147934 <chandiofatima02@gmail.com>
2026-09-12 18:12:15 +0500

    Update CourseDetails.jsx

--- frontend/my-project/src/pages/CourseDetails.jsx
+++ frontend/my-project/src/pages/CourseDetails.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 
-const API_URL = "https://localhost:5000/api/courses";
+const API_URL = "https://learnhub-backend-pi.vercel.app/api/courses";
 
 function CourseDetails(){
     const {id} = useParams();
