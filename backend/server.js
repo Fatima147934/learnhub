a34d5a446e09acad52edfdf487a8fdfb90ece794
Fatima147934 <chandiofatima02@gmail.com>
2026-09-12 17:54:59 +0500

    update

--- backend/server.js
+++ backend/server.js
@@ -24,11 +24,12 @@ app.get('/', (req, res)=>{
     res.send('LearnHub API is running');
 });
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI).then(()=>{
     console.log('MongoDB Altas connected');
-    app.listen(PORT, ()=> console.log(`server running on http://localhost:${PORT}`));
+    // app.listen(PORT, ()=> console.log(`server running on http://localhost:${PORT}`));
 })
 
 .catch((error)=> console.error('MongoDB connection error:', error.message));
+module.exports= app;
